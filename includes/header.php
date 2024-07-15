2f5df39cabfbb6127ef310b490e1ea3136517c2f
jinhao <jinhao@example.com>
2024-07-15 10:46:15 +0800

    added header

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<header>
+    <div>
+        <div>
+            <a href="/">
+                <img src="" alt="Logo is loading">
+                <h1>Mondstadt Market</h1>
+            </a>
+        </div>
+
+        <div id="search-bar">
+            <input type="text">
+        </div>
+        <div id="cart">
+            <a href="">
+                <img src="" alt="Cart image is loading">
+            </a>
+        </div>
+        
+    </div>
+</header>
