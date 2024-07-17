c42d0f0aec187144015d86ad665e0f6180ffd568
Sodiumchloridy <67376832+Sodiumchloridy@users.noreply.github.com>
2024-07-17 10:41:18 +0800

    update header styling

--- index.php
+++ index.php
@@ -2,15 +2,18 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Mondstadt Market </title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        crossorigin="anonymous" referrerpolicy="no-referrer" /> <!-- Fontawesome icons -->
+    <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/header.css">
 </head>
 
 <body>
-    <?php include("includes/header.php"); ?>
+    <?php include ("includes/header.php"); ?>
 </body>
 
 </html>
