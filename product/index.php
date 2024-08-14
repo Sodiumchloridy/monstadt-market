9ca415d647b8579ecdf52af7c0975fdcd119a393
cscheah5 <cheahchoonsam5@gmail.com>
2024-08-14 22:03:43 +0800

    product

--- product/index.php
+++ product/index.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/header.css">
     <link rel="stylesheet" href="styles/footer.css">
+    <link rel="stylesheet" href="styles/product.css">
 </head>
 
 <body>
@@ -29,16 +30,26 @@
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
 
+    echo '<div class="product-container">';
+    echo '<div class="product-card">';
     if (mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
+        echo "<div class='product-col-image'>";
         echo "<img src='images/" . $row['prod_img_name'] . "' alt='" . $row['prod_name'] . "'>";
+        echo "</div>";
+
+        echo "<div class='product-col-details'>";
         echo "<h1 align='center'>" . $row['prod_name'] . "</h1>";
         echo "<p align='center'>Description: " . $row['prod_desc'] . "</p>";
         echo "<p align='center'>Price: " . $row['prod_price'] . "</p>";
         echo "<p align='center'>Region: " . $row['prod_region'] . "</p>";
         echo "<p align='center'>Available: " . $row['prod_numAvailable'] . "</p>";
         echo "<p align='center'>Sold: " . $row['prod_numSold'] . "</p>";
+        echo "</div>";
     }
+    echo '</div>';
+
+    echo '</div>';
     ?>
 </body>
 
