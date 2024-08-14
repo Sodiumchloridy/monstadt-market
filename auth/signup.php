ee508b2a3737c63b4cbe10e2853e66a48792b7e7
J1nnH <125129010+J1nnH@users.noreply.github.com>
2024-08-14 15:28:43 +0800

    added image input and default profile pic

--- auth/signup.php
+++ auth/signup.php
@@ -43,18 +43,33 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     if(empty($_POST['unit'])) {
         $unitErr = "Unit is required";
         $addressErr[] = $unitErr;
+    } else {
+        $unit = test_input($_POST['unit']);
     }
     if(empty($_POST['street'])) {
         $streetErr = "Street is required";
         $addressErr[] = $streetErr;
+    } else {
+        $street = test_input($_POST['street']);
     }
     if(empty($_POST['poskod'])) {
         $poskodErr = "Poskod is required";
         $addressErr[] = $poskodErr;
+    } else {
+        $poskod = test_input($_POST['poskod']);
     }
     if(empty($_POST['state'])) {
         $stateErr = "State is required";
         $addressErr[] = $stateErr;
+    } else {
+        $state = test_input($_POST['state']);
+    }
+    if(isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+        $imgTmpPath = $_FILES['image']['tmp_name'];
+        $imgData = file_get_contents($imgTmpPath);
+    } else {
+        // use default picture if no file is uploaded
+        $imgData = file_get_contents("../default_images/default_profile.jpg");
     }
 
     if(count($addressErr) === 0 && empty($nameErr) && empty($passErr) && empty($emailErr) && empty($phoneErr)) {
@@ -72,17 +87,16 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $state = test_input($_POST['state']);
         $address = "$unit $street $poskod $state";
 
-        
+        $stmt = mysqli_prepare($conn, "INSERT INTO users (u_username, u_password, u_email, u_address, u_phone, u_profile_pic) VALUES (?, ?, ?, ?, ?, ?)");
+        mysqli_stmt_bind_param($stmt, "ssssss", $name, $hashedPass, $email, $address, $phone, $imgData);
 
-        $stmt = mysqli_prepare($conn, "INSERT INTO users (u_username, u_password, u_email, u_address, u_phone) VALUES (?, ?, ?, ?, ?)");
-        mysqli_stmt_bind_param($stmt, "sssss", $name, $hashedPass, $email, $address, $phone);
-
-        
+    
         if(mysqli_stmt_execute($stmt)) {
             $message = "Record inserted successfully";
         } else {
             $message = "Error inserting record: " . mysqli_error($conn);
         }
+        
 
         // closing statement and connection
         mysqli_stmt_close($stmt);
@@ -107,7 +121,7 @@ function test_input($data) {
     <title>Sign Up</title>
 </head>
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" id="signupForm">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" enctype="multipart/form-data" id="signupForm">
         <h2>Sign Up</h2>
         <?php echo $message;?>
         <table>
@@ -154,25 +168,30 @@ function test_input($data) {
             <tr>
                 <td>
                     <select name="state" required>
-                        <option value="" disabled selected>Select State</option>
-                        <option value="Johor">Johor</option>
-                        <option value="Kedah">Kedah</option>
-                        <option value="Kelantan">Kelantan</option>
-                        <option value="Kuala Lumpur">Kuala Lumpur</option>
-                        <option value="Labuan">Labuan</option>
-                        <option value="Melaka">Melaka</option>
-                        <option value="Negeri Sembilan">Negeri Sembilan</option>
-                        <option value="Pahang">Pahang</option>
-                        <option value="Penang">Penang</option>
-                        <option value="Perak">Perak</option>
-                        <option value="Perlis">Perlis</option>
-                        <option value="Putrajaya">Putrajaya</option>
-                        <option value="Sabah">Sabah</option>
-                        <option value="Sarawak">Sarawak</option>
-                        <option value="Selangor">Selangor</option>
-                        <option value="Terengganu">Terengganu</option>
+                        <option value="" disabled <?php if ($state == "") echo 'selected'; ?> >Select State</option>
+                        <option value="Johor" <?php if ($state == "Johor") echo 'selected'; ?>>Johor</option>
+                        <option value="Kedah" <?php if ($state == "Kedah") echo 'selected'; ?>>Kedah</option>
+                        <option value="Kelantan" <?php if ($state == "Kelantan") echo 'selected'; ?>>Kelantan</option>
+                        <option value="Kuala Lumpur" <?php if ($state == "Kuala Lumpur") echo 'selected'; ?>>Kuala Lumpur</option>
+                        <option value="Labuan" <?php if ($state == "Labuan") echo 'selected'; ?>>Labuan</option>
+                        <option value="Melaka" <?php if ($state == "Melaka") echo 'selected'; ?>>Melaka</option>
+                        <option value="Negeri Sembilan" <?php if ($state == "Negeri Sembilan") echo 'selected'; ?>>Negeri Sembilan</option>
+                        <option value="Pahang" <?php if ($state == "Pahang") echo 'selected'; ?>>Pahang</option>
+                        <option value="Penang" <?php if ($state == "Penang") echo 'selected'; ?>>Penang</option>
+                        <option value="Perak" <?php if ($state == "Perak") echo 'selected'; ?>>Perak</option>
+                        <option value="Perlis" <?php if ($state == "Perlis") echo 'selected'; ?>>Perlis</option>
+                        <option value="Putrajaya" <?php if ($state == "Putrajaya") echo 'selected'; ?>>Putrajaya</option>
+                        <option value="Sabah" <?php if ($state == "Sabah") echo 'selected'; ?>>Sabah</option>
+                        <option value="Sarawak" <?php if ($state == "Sarawak") echo 'selected'; ?>>Sarawak</option>
+                        <option value="Selangor" <?php if ($state == "Selangor") echo 'selected'; ?>>Selangor</option>
+                        <option value="Terengganu" <?php if ($state == "Terengganu") echo 'selected'; ?>>Terengganu</option>
                     </select>
                     <div class="error"><?php echo $stateErr?></div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan = "3">
+                    <input type="file" accept="image/*">
                 </td>
             </tr>
         </table>
