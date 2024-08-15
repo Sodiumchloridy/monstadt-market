fb48939c893d143c39dc255c62382b2a8f6e0083
cscheah5 <cheahchoonsam5@gmail.com>
2024-08-15 11:01:33 +0800

    test

--- product/validate_number.js
+++ product/validate_number.js
@@ -1,1 +1,18 @@
-const numericInputs = document.querySelectorAll("[inputmode")
+const numericInputs = document.querySelectorAll("[inputmode='numeric']");
+
+numericInputs.forEach((input) =>{
+    validateInput(input);
+})
+
+function validateInput(el){
+    el.addEventListener("beforeInput", function(e){
+        let beforeValue = el.value;
+        e.target.addEventListener("input", function(){
+            if (el.validity.patternMismatch){
+                el.value = beforeValue;
+            }
+        }, {once : true}
+    );
+    });
+
+}
