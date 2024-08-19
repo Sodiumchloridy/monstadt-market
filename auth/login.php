b9a1824f7d628f57add822e6c89e1315b0c5f809
J1nnH <125129010+J1nnH@users.noreply.github.com>
2024-08-20 00:32:51 +0800

    Temp resolution: added prevention to login page

--- auth/login.php
+++ auth/login.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+if(isset($_SESSION['user_id'])){
+    die("You already logged in");
+}
+
 $name = $pass = "";
 $nameErr = $passErr = "";
 $validName = $validPass = false;
