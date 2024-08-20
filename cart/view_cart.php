133e98211cf661dd02cd46951b0759fa26517186
J1nnH <125129010+J1nnH@users.noreply.github.com>
2024-08-20 16:39:03 +0800

    added image to cart

--- cart/view_cart.php
+++ cart/view_cart.php
@@ -10,7 +10,7 @@ include("../config/config.php");
 
 // query to get cart items
 $stmt = mysqli_prepare($conn, "
-SELECT p.prod_id, p.prod_name, p.prod_price, c.quantity
+SELECT p.prod_id, p.prod_name, p.prod_price, p.prod_img_name, c.quantity
 FROM cart c
 JOIN product p ON c.prod_id = p.prod_id
 WHERE c.u_id = ?
@@ -18,7 +18,7 @@ WHERE c.u_id = ?
 
 mysqli_stmt_bind_param($stmt, "s", $_SESSION['user_id']);
 mysqli_stmt_execute($stmt);
-mysqli_stmt_bind_result($stmt, $prodId, $prodName, $prodPrice, $prodQuantity);
+mysqli_stmt_bind_result($stmt, $prodId, $prodName, $prodPrice, $prodImgName, $prodQuantity);
 mysqli_stmt_store_result($stmt);
 
 // fetch the result and display them
@@ -26,9 +26,9 @@ if(mysqli_stmt_num_rows($stmt) === 0) {
     echo "<p>Your cart is empty</p>";
     echo "<a href='../'>Add item to cart</a>";
 } else {
-    echo "<h2>Your cart</h2>";
     while(mysqli_stmt_fetch($stmt)) {
-        echo "<div>";
+        echo "<div class='cart-item'>";
+        echo "<img src='../images/" . htmlspecialchars($prodImgName) . "'/>";
         echo "<p>Product: " . htmlspecialchars($prodName) . "</p>";
         echo "<p>Price: $" . htmlspecialchars($prodPrice) . "</p>";
         echo "<p>Quantity: " . htmlspecialchars($prodQuantity) . "</p>";
