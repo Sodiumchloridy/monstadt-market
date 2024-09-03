e2a1e052bea2f0c7947d47af9651a4691693d068
Sodiumchloridy <67376832+Sodiumchloridy@users.noreply.github.com>
2024-09-03 15:16:30 +0800

    style: Improve product page layout and design

--- product/index.php
+++ product/index.php
@@ -1,10 +1,20 @@
-<?php 
+<?php
 session_start();
 if (!isset($_SESSION['user_id'])) {
     $_SESSION['redirect_after_login'] = $_SERVER['REQUEST_URI'];
     header("Location: ../auth/login.php");
     exit();
 }
+
+$item = $_GET['id'] ?? null;
+$item or die('<h1 align="center">Please select an item.</h1>');
+
+// Fetch content
+include('../config/config.php');
+$stmt = mysqli_prepare($conn, "SELECT * FROM `product` WHERE `prod_id` = ?");
+mysqli_stmt_bind_param($stmt, "s", $item);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
 ?>
 
 
@@ -23,67 +33,55 @@ if (!isset($_SESSION['user_id'])) {
 
 <body>
     <?php
-    $item = $_GET['id'] ?? null;
-
     // Header
     include('../includes/header.php');
-
-    $item or die('<h1 align="center">Please select an item.</h1>');
-
-    // Fetch content
-    include('../config/config.php');
-    $stmt = mysqli_prepare($conn, "SELECT * FROM `product` WHERE `prod_id` = ?");
-    mysqli_stmt_bind_param($stmt, "s", $item);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-
-
     if (mysqli_num_rows($result) > 0) {
         echo '<div class="product-container">';
-            echo '<div class="product-card">';
 
-            $row = mysqli_fetch_assoc($result);
-            echo "<section class='product-col-image'>";
-                echo "<img src='../images/" . $row['prod_img_name'] . "' alt='" . $row['prod_name'] . "'>";
-            echo "</section>";
+        $row = mysqli_fetch_assoc($result);
+        $prod_name = $row["prod_name"];
+        echo "<section class='product-col-image'>";
+        echo "<img src='../images/" . $row['prod_img_name'] . "' alt='" . $row['prod_name'] . "'>";
+        echo "<h1 align='center' data-content='$prod_name'>" . $row['prod_name'] . "</h1>";
+        echo "</section>";
 
-            echo "<section class='product-col-details'>";
-                echo "<h1 align='center'>" . $row['prod_name'] . "</h1>";
-                echo "<p align='center'>Description: " . $row['prod_desc'] . "</p>";
-                echo "<p align='center'>Price: " . $row['prod_price'] . "</p>";
-                echo "<p align='center'>Region: " . $row['prod_region'] . "</p>";
-                echo "<p align='center'>Available: " . $row['prod_numAvailable'] . "</p>";
-                echo "<p align='center'>Sold: " . $row['prod_numSold'] . "</p>";
-            ?>
-            <!-- action="../monstadt-market/cart/add_to_cart.php" -->
-            <form id="add-to-cart-form" action="../cart/add_to_cart.php" method="post">
-                <!--Quantity -->
-                <label for="quantity">Quantity: </label>
-                <span class="qty-container">
-                    <button type="button" class="quantity-button" id="decrease-quantity" aria-label="Decrease quantity">
-                        <i class="fa-regular fa-minus"></i>
-                    </button>
+        echo "<section class='product-col-details'>";
+        echo "<h1 align='center' data-content='$prod_name'>" . $row['prod_name'] . "</h1>";
+        echo "<p align='justify'>Description: " . $row['prod_desc'] . "</p>";
+        echo "<div class='details-grid'>";
+        echo "<p>Price: " . $row['prod_price'] . "</p>";
+        echo "<p>Region: " . $row['prod_region'] . "</p>";
+        echo "<p>Available: " . $row['prod_numAvailable'] . "</p>";
+        echo "<p>Sold: " . $row['prod_numSold'] . "</p>";
+        echo "</div>";
+    ?>
+        <!-- action="../monstadt-market/cart/add_to_cart.php" -->
+        <form id="add-to-cart-form" action="../cart/add_to_cart.php" method="post">
+            <!--Quantity -->
+            <label for="quantity">Quantity: </label>
+            <span class="qty-container">
+                <button type="button" class="quantity-button" id="decrease-quantity" aria-label="Decrease quantity">
+                    <i class="fa-regular fa-minus"></i>
+                </button>
 
-                    <input type="text" id="quantity" name="quantity" inputmode="numeric" pattern="[0-9]+" autocomplete="off" value="1">
+                <input type="text" id="quantity" name="quantity" inputmode="numeric" pattern="[0-9]+" autocomplete="off" value="1">
 
-                    <button type="button" class="quantity-button" id="increase-quantity" aria-label="Increase quantity">
-                        <i class="fa-regular fa-plus"></i>
-                    </button>
-                </span>
-                <div class="error" id="quantity-error"></div>
+                <button type="button" class="quantity-button" id="increase-quantity" aria-label="Increase quantity">
+                    <i class="fa-regular fa-plus"></i>
+                </button>
+            </span>
+            <div class="error" id="quantity-error"></div>
 
-                <!-- Product id -->
-                <input type="hidden" name="product_id" value="<?php echo htmlspecialchars($row['prod_id'])?>">
-                <!-- Max Available -->
-                <input type="hidden" id="maxAvailable" value="<?php echo $row['prod_numAvailable']; ?>">
+            <!-- Product id -->
+            <input type="hidden" name="product_id" value="<?php echo htmlspecialchars($row['prod_id']) ?>">
+            <!-- Max Available -->
+            <input type="hidden" id="maxAvailable" value="<?php echo $row['prod_numAvailable']; ?>">
 
-                <input type="submit" value="Add to cart">
-            </form>
+            <input type="submit" value="Add to cart">
+        </form>
 
-            <?php
-            echo "</section>";
-
-            echo '</div>';
+    <?php
+        echo "</section>";
         echo '</div>';
     }
 
